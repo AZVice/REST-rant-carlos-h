3645b48263ceaea60f9923e344baeeb35176af17
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-11-13 22:32:03 -0800

    working on bonus tasks part IV

--- views/home.jsx
+++ views/home.jsx
@@ -5,16 +5,17 @@ function home() {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1>REST-Rant</h1>
                 <div>
                     <img src="./images/chia-fruit-drink.jpg" alt="Chia Fruit Shake"/>
                     </div>
                         Photo by <a href='https://unsplash.com/photos/two-fruit-beverages-on-glass-cups-MsTOg6rhRVk'>Brenda Godinez</a> on <a href='https://unsplash.com/@cravethebenefits'>Unsplash</a>
                     <div>
                 </div>
-                <a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
+                <a href="/places"><button className="btn-primary">Places Page</button></a>
+                <div>
+                <a href='/places'>Explore Places</a>
+                </div>
 
             </main>
         </Def>

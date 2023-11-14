627202f83d32f4552389de9a76207f5ceba26c33
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-11-13 22:51:25 -0800

    Completed bonus for REST-rant IV

--- controllers/places.js
+++ controllers/places.js
@@ -16,7 +16,21 @@ router.get('/', (req, res) => {
         state: 'NM',
         cuisines: 'Steak, Bakery',
         pic: '/images/rest2.jpg',  //Photo by Prithviraj A. from Unsplashed.com. https://unsplash.com/photos/a-restaurant-filled-with-wooden-tables-and-chairs-vDlt9BQND-o
-      }];
+      },
+    {
+        name: 'Fresh Air Restaurant',
+        city: 'Berlin',
+        state: 'Brandenburg',
+        cuisines: 'Seafood, Peruvian Ceviche',
+        pic: '/images/rest3.jpg',  //Photo by Kate Holovacheva from Pexels.com. https://www.pexels.com/photo/vendors-in-front-of-a-government-building-5037185/
+     },  
+    {
+        name: 'Candy Shop',
+        city: 'Buena Park',
+        state: 'CA',
+        cuisines: 'Sugar and more sugar',
+        pic: '/images/candy-shop.jpg',  //Photo by Linken Van Zyl from Pexels.com. https://www.pexels.com/photo/close-up-of-colorful-candy-18958642/
+  }];
     res.render('places/index', {places})
 });
 

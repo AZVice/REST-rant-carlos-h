aa5eef62910fc7cc95d595b726c46385abe2a193
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-11-14 19:10:15 -0800

    New Places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,6 @@
 //file places.js
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 
 
@@ -8,40 +9,21 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  if(!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state){
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
   
 });
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'Baklava Restaurant',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Desserts',
-        pic: './images/rest1.jpg',  // Photo by Optical Chemist from Unsplash.com https://unsplash.com/photos/a-group-of-people-walking-down-a-street-at-night-uz7dXgkdvkU
-      }, 
-    {
-        name: 'Open Flame Restaurant',
-        city: 'Las Cruzes',
-        state: 'NM',
-        cuisines: 'Steak, Bakery',
-        pic: '/images/rest2.jpg',  //Photo by Prithviraj A. from Unsplashed.com. https://unsplash.com/photos/a-restaurant-filled-with-wooden-tables-and-chairs-vDlt9BQND-o
-      },
-    {
-        name: 'Fresh Air Restaurant',
-        city: 'Berlin',
-        state: 'Brandenburg',
-        cuisines: 'Seafood, Peruvian Ceviche',
-        pic: '/images/rest3.jpg',  //Photo by Kate Holovacheva from Pexels.com. https://www.pexels.com/photo/vendors-in-front-of-a-government-building-5037185/
-     },  
-    {
-        name: 'Candy Shop',
-        city: 'Buena Park',
-        state: 'CA',
-        cuisines: 'Sugar and more sugar',
-        pic: '/images/candy-shop.jpg',  //Photo by Linken Van Zyl from Pexels.com. https://www.pexels.com/photo/close-up-of-colorful-candy-18958642/
-  }];
     res.render('places/index', {places})
 });
 

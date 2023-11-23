c0593638465f3f1d42a31adf9d2c11cae1f79e30
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-11-22 16:34:14 -0800

    Added connection status to app

--- models/index.js
+++ models/index.js
@@ -7,3 +7,23 @@ mongoose.connect(process.env.MONGO_URI, {
 })
 
 module.exports.Place = require('./places')
+
+const db = mongoose.connection;
+
+
+
+//This code below is not from homwork.  
+
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB!');
+});
+
+
+if (mongoose.connection.readyState === 1) {   // Check if Mongoose is connected
+  console.log('Mongoose is running.');
+} else {
+  console.log('Mongoose is not running.');
+}
+
+module.exports.Place = require('./places');
